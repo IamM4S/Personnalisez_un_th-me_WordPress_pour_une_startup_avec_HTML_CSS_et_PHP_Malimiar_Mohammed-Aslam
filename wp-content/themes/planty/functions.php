8847480952681bbd8d2351a8a2b98276a5592601
IamM4S <aslam.malimiar@outlook.com>
2024-06-18 14:19:39 +0200

    Completed

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -7,14 +7,6 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
-add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
-function add_extra_item_to_nav_menu( $items, $args ) {
-	if (is_user_logged_in() && $args->menu === 4) {
-		$items .= '<li class="wt_menu_item_user_avatar"><a href="/my-account">' . get_avatar( $current_user->ID, 32 ) . '</a></li>';
-	}
-	return $items;
-}
-
 function my_wp_nav_menu_args( $args = '' ) {
     if( is_user_logged_in() ) { 
         $args['menu'] = 'logged-in';
